b9fccc9661f819c72d8eeef76df7cc8de5546a9e
ruvik2000 <ruvikperera2000@gmail.com>
2023-12-08 15:34:10 +0530

    added gcloud changes

--- index.js
+++ index.js
@@ -13,10 +13,14 @@ app.use(cors())
 
 app.use('/user', userRoutes)
 
+app.get('/', (req, res) => {
+    res.send('<h1>Wellcome to soultrain API</h1> <p>Start by accessing /user/allUsers API</p>');
+});
+
 // connection URL for mongoBD atlas(cloud database)
 const CONNECTION_URL = 'mongodb+srv://ruvikperera:abcd1234@soultrain.c8ddrij.mongodb.net/?retryWrites=true&w=majority'
 
-const PORT = process.PORT || 5000
+const PORT = process.PORT || 8080
 
 // make the conection to the databse and start the server
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
